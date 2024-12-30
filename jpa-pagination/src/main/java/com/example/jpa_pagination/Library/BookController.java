0bfec073c58cd201f456b5b32537480ba0718d1f
SrijaG29 <srijag298@gmail.com>
2024-12-30 20:56:36 +0530

    added getmapping in pagination

--- jpa-pagination/src/main/java/com/example/jpa_pagination/Library/BookController.java
+++ jpa-pagination/src/main/java/com/example/jpa_pagination/Library/BookController.java
@@ -1,9 +1,10 @@
 package com.example.jpa_pagination.Library;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,4 +20,15 @@ public class BookController {
         bookRepository.saveAll(books);
         return "All books added";
     }
+
+    @GetMapping("/books")
+    public Page<BookEntity> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "BookName") String sortByValue
+    ){
+        PageRequest pageRequest = PageRequest.of(page,size, Sort.by(sortByValue));
+        return bookRepository.findAll(pageRequest);
+    }
+
 }
